0105449529e5b3d06c28f28147ec82517098873d
ehsandada <ehsandada2473@gmail.com>
2022-04-29 22:11:37 +0430

    upload data types assingment

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-let userAge= prompt("enter your age")
-let userName= prompt("enter your name")
-let userSex= prompt("enter your sex")
-let userAgeNumber=Number(userAge)
-alert(userAgeNumber + ' ' +userName  + ' ' + userSex)
+let firstName = prompt("enter your first name: ")
+    checkAnswer(firstName)
+let lastName = prompt("Enter your last name: ")
+    checkAnswer(lastName)
+let age = prompt("How old are you?" )
+    checkAnswer(age)
+let gender= prompt("what is your gender?")
+    checkAnswer(gender)
+alert(`fist name: ${firstName} , last name: ${lastName} , age: ${+age} , gender: ${gender}`)
+
+
+function checkAnswer(any){
+    if (any ===null || any==="") {
+        alert(`You did not fill the box.`);
+      }
+    
+}
+
+document.getElementById("firstname").innerHTML = firstName
+document.getElementById("lastname").innerHTML = lastName
+document.getElementById("age").innerHTML = age
+document.getElementById("gender").innerHTML = gender
